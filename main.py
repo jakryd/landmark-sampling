c614e70eb72bb9edfe41920c4d9c3b35e9154cd6
karolina28469 <44229749+karolina28469@users.noreply.github.com>
2022-01-17 10:45:30 +0100

    Random sampling

--- main.py
+++ main.py
@@ -1,192 +1,47 @@
-from random import randrange
-import math
-import random
+import numpy as np
 
-N = 6
-arr = [[0 for x in range(N)] for y in range(N)]
-arr[0][1] = 10
-arr[0][2] = 15
-arr[0][3] = 20
-arr[0][4] = 25
-arr[0][5] = 30
-arr[1][2] = 35
-arr[1][3] = 25
-arr[1][4] = 45
-arr[1][5] = 15
-arr[2][3] = 30
-arr[2][4] = 10
-arr[2][5] = 25
-arr[3][4] = 35
-arr[3][5] = 20
-arr[4][5] = 15
+def random_sampling(n_sample, logweight_tensor_, n_landmark):
+  t_weight = sum(np.exp(logweight_tensor_))
+  running_t_weight = 0
 
-for i in range(N):
-    for j in range(N):
-        arr[j][i] = arr[i][j]
+  landmark_indices = []
+  selected = np.full((n_sample), False, dtype=bool)
 
+  n_count = 0
 
-def swap(list, pos1, pos2):
-    list[pos1], list[pos2] = list[pos2], list[pos1]
-    return list
+  while(n_count < n_landmark):
+    tw = 0
+    r01 = np.random.rand()
+    r = (t_weight - running_t_weight) * r01
 
-def shuffle(list):
-    for i in range(100):
-        swap(list, randrange(N), randrange(N))
-    return list
+    for j in range(n_sample):
+      if(selected[j] == False): 
+        tw += np.exp(logweight_tensor_[j])
+        if(r < tw):
+          selected[j] = True
+          landmark_indices.append(j)
+          running_t_weight += np.exp(logweight_tensor_[j])
 
-def cost(list):
-    cost = 0
+          break
+    n_count += 1
 
-    for i in range(N):
-        a = 0
-        b = 0
-        a = list[i]
-        if (i + 1 == N):
-            b = list[0]
-        else:
-            b = list[i + 1]
-        cost += arr[a][b]
-    return cost
-
-def calc_fitness(list,pop_size):
-    fitness = []
-    for i in range(pop_size):
-        print(list[i], (cost(list[i])))
-        fitness.append(1/(cost(list[i]) + 1)) ## TODO 0
-    return fitness
-
-def normalize_fitness(fitness, pop_size):
-    sum = 0
-    for i in range(pop_size):
-        sum += fitness[i]
-    for i in range(pop_size):
-        fitness[i] = fitness[i]/sum
-    return fitness
-
-def max_val(list, population, pop_size):
-    max = -math.inf
-    num = 0
-    for i in range(pop_size):
-        if list[i] > max:
-            max = list[i]
-            num = i
-    return population[num]
-
-def pick_one(list, prob):
-    index = 0
-    r = random.uniform(0, 1)
-
-    while(r > 0):
-        r = r - prob[index]
-        index += 1
-
-    index -= 1
-    return list[index]
-
-def mutate(order):
-    order = swap(order, randrange(N), randrange(N))
-    return order
-
-def nextGeneration(population, fitness, pop_size):
-    newPopulation = [[0 for x in range(N)] for y in range(pop_size)]
-    best = math.inf
-    best_route = []
-
-    for _ in range(5):
-        for i in range(pop_size):
-            order = pick_one(population, fitness)
-            c = cost(order)
-            print(order, c)
-
-            if(best > c) :
-                best = c
-                best_route = order
-
-            order = mutate(order)
-            print(order, cost(order))
-
-            newPopulation[i] = order
-            print()
-
-        population = newPopulation
-
-    print("BEST", best_route, "cost", best)
-
-
-def gen_population(list, num):
-    population = []
-    for i in range(num):
-        temp = shuffle(list)[:]
-        ##print("temp", temp)
-        population.append(temp)
-        ##print("pop", population)
-    return population
+  return landmark_indices
 
 def main():
+    X = np.loadtxt("colvar-1D.data")
+    N = len(X)
+    loc = [row[1] for row in X]
+    lw = [row[2] for row in X]
 
-    ##read & transform data
-    f = open("colvar-1D.data", "r")
-    data = []
-    data = [line.split() for line in f]
-    print("numbr of data:", len(data))
-    N = len(data)
-    N = 11
-    data2 = [data[i] for i in range(1, N)]
-    ##print(data2)
+    ##data2 = [X[i] for i in range(1, 10)]
+    ##loc = [row[1] for row in data2]
+    ##lw = [row[2] for row in data2]
 
-    loc = [row[1] for row in data2]
-    loc = [float(i) for i in loc]
+    n_sample = len(X)
+    logweight_tensor_ = lw
 
-    max_lw = -10.0
-    lw = [row[2] for row in data2]
-    lw = [float(i) for i in lw]
-    print("\nlog(weight)column:")
-    print(lw)
-    l = len(lw)
-
-    for i in range(l):
-        if (max_lw < lw[i]):
-            max_lw = lw[i]
-    print("max log(weight):", max_lw)
-
-    for i in range(l):
-        lw[i] -= max_lw
-    print("log(weight)column - max log(weight):")
-    print(lw)
-
-    weight = lw
-    for i in range(l):
-        weight[i] = math.exp(weight[i])
-    print("weight = exp(log(weight)):")
-    print(weight)
-
-    print("location:")
-    print(loc)
-    print("#########")
-
-    ## ---
-    cities = [i for i in range(N)]
-    print(cities)
-
-    cities = shuffle(cities)
-    print("koszt")
-    print(cost(cities))
-    print()
-
-    pop_size = 10
-    population = gen_population(cities, pop_size)
-    print("gen pop", population)
-
-    fitness = calc_fitness(population, pop_size)
-    print("\nfitness", fitness)
-    m = max_val(fitness, population, pop_size)
-    print("best", m)
-    norm_fitness = normalize_fitness(fitness, pop_size)
-    print("fitness in %", norm_fitness)
-
-    print("\ngenerations")
-    print(nextGeneration(population, norm_fitness, pop_size))
-
+    ndx = random_sampling(n_sample, logweight_tensor_, 100)
+    print(ndx)
 
 if __name__ == "__main__":
     main()
